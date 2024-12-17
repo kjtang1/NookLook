0fc9fca940cb8250d16b6397119e24fed5b9bc51
kjtang1 <kjtang1@asu.edu>
2024-12-17 15:54:07 -0700

    set up project directory

---
+++ backend/app.py
@@ -0,0 +1,4 @@
+import requests
+import psycopg2
+import Flask
+
---
+++ database/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE villager (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    species VARCHAR(50),
+    personality VARCHAR(50),
+    icon_url VARCHAR(100)
+);
