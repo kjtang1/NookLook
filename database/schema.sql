6ae55af31de7a79b3243f5602f2d3dad0cc7626f
kjtang1 <kjtang1@asu.edu>
2024-12-17 17:30:49 -0700

    updated villager table to store more info

--- database/schema.sql
+++ database/schema.sql
@@ -1,7 +1,11 @@
 CREATE TABLE villager (
     id SERIAL PRIMARY KEY,
     name VARCHAR(50),
-    species VARCHAR(50),
+    gender VARCHAR(10),
     personality VARCHAR(50),
-    icon_url VARCHAR(100)
+    species VARCHAR(50),
+    birthday VARCHAR(20)
+    catchphrase VARCHAR(100),
+    icon_uri VARCHAR(100),
+    image_uri VARCHAR(100),
 );
